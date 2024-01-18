7b54283e19b2d82e4eb0d3dcf1d42e2aa626b391
Curtis Polic <141887991+curtispolic@users.noreply.github.com>
2024-01-19 10:19:14 +1100

    First Code
    Hello World

---
+++ WriteYourFirstCode.cs
@@ -0,0 +1,9 @@
+﻿// Old faithful
+Console.WriteLine("Hello, World!");
+
+// WriteLine appends /n at the end while Write doesn't
+Console.WriteLine();
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+
+// "" is for string literals while '' is for char literals

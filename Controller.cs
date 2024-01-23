525584d1d3b27be04d65bc0c9503f5b24c76f85f
Curtis Polic <curtis.polic@gmail.com>
2024-01-24 10:59:57 +1100

    Controller updates

--- Controller.cs
+++ Controller.cs
@@ -13,47 +13,119 @@
                 {
                     case "1":
                         // Will display all the outputs for the entire first set
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the WriteYourFirstCode module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-1 WriteYourFirstCode module:");
                         WriteYourFirstCode.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the LiteralAndVariableValues module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-2 LiteralAndVariableValues module:");
                         LiteralAndVariableValues.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the BasicStringFormatting module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-3 BasicStringFormatting module:");
                         BasicStringFormatting.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the BasicNumberOperations module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-4 BasicNumberOperations module:");
                         BasicNumberOperations.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the CalculateAndPrintStudentGrades module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-5-1 CalculateAndPrintStudentGrades module:");
                         CalculateAndPrintStudentGrades.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the CalculateAndPrintStudentGradesBetter module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-5-2 CalculateAndPrintStudentGradesBetter module:");
                         CalculateAndPrintStudentGradesBetter.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the CalculateFinalGPA module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-6 CalculateFinalGPA module:");
                         CalculateFinalGPA.Run();
 
                         break;
 
                     case "2":
                         // Will display all the outputs for the entire second set
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the CallMethodsFromDotnetLibrary module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-1 CallMethodsFromDotnetLibrary module:");
                         CallMethodsFromDotnetLibrary.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the DecisionLogic module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-2 DecisionLogic module:");
                         DecisionLogic.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the StoreAndIterateOverArrays module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-3 StoreAndIterateOverArrays module:");
                         StoreAndIterateOverArrays.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the ForeachAndIfElseStructures module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-4 ForeachAndIfElseStructures module:");
                         ForeachAndIfElseStructures.Run();
 
-                        Console.WriteLine("\n--------------------\n\nBelow is the output for the FurtherForeachAndIfElseStructures module:");
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-5 FurtherForeachAndIfElseStructures module:");
                         FurtherForeachAndIfElseStructures.Run();
 
                         break;
+
+                    case "1-1":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-1 WriteYourFirstCode module:");
+                        WriteYourFirstCode.Run();
+
+                        break;
+
+                    case "1-2":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-2 LiteralAndVariableValues module:");
+                        LiteralAndVariableValues.Run();
+
+                        break;
+
+                    case "1-3":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-3 BasicStringFormatting module:");
+                        BasicStringFormatting.Run();
+
+                        break;
+
+                    case "1-4":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-4 BasicNumberOperations module:");
+                        BasicNumberOperations.Run();
+
+                        break;
+
+                    case "1-5-1":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-5-1 CalculateAndPrintStudentGrades module:");
+                        CalculateAndPrintStudentGrades.Run();
+
+                        break;
+
+                    case "1-5-2":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-5-2 CalculateAndPrintStudentGradesBetter module:");
+                        CalculateAndPrintStudentGradesBetter.Run();
+
+                        break;
+
+                    case "1-6":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 1-6 CalculateFinalGPA module:");
+                        CalculateFinalGPA.Run();
+
+                        break;
+
+                    case "2-1":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-1 CallMethodsFromDotnetLibrary module:");
+                        CallMethodsFromDotnetLibrary.Run();
+
+                        break;
+                    
+                    case "2-2":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-2 DecisionLogic module:");
+                        DecisionLogic.Run();
+
+                        break;
+
+                    case "2-3":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-3 StoreAndIterateOverArrays module:");
+                        StoreAndIterateOverArrays.Run();
+
+                        break;
+
+                    case "2-4":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-4 ForeachAndIfElseStructures module:");
+                        ForeachAndIfElseStructures.Run();
+
+                        break;
+
+                    case "2-5":
+                        Console.WriteLine("\n--------------------\n\nBelow is the output for the 2-5 FurtherForeachAndIfElseStructures module:");
+                        FurtherForeachAndIfElseStructures.Run();
+
+                        break;                 
 
                     default:
                         Console.WriteLine($"Invalid argument \"{arg}\"");
